8cb1f5f8c71e9e8a620dc085c71a4c33056386a9
Vincenzo Russo <v@artetecha.com>
2017-01-17 15:39:35 +0000

    Bugfix

--- src/Command/Drupal/DrupalUncleanFeaturesCommand.php
+++ src/Command/Drupal/DrupalUncleanFeaturesCommand.php
@@ -44,7 +44,7 @@ class DrupalUncleanFeaturesCommand extends ExtendedCommandBase {
     // before running the process.
     putenv('COLUMNS=512');
     $alias = basename(realpath($input->getArgument('directory')));
-    $p = new Process("drush @$alias._local--$app->getId() fl --status=enabled");
+    $p = new Process("drush @$alias._local--" . $app->getId() . " fl --status=enabled");
     $p->setTimeout(self::$config->get('local.deploy.external_process_timeout'));
     try {
       $p->mustRun();
