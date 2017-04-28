89bf06908379c6018c890e19ddbc43c03deda367
Vincenzo Russo <v@artetecha.com>
2017-04-28 16:41:15 +0000

    Make DrupalRemoteFilesMountCommand work with latest CLI.

--- src/Command/Drupal/DrupalRemoteFilesMountCommand.php
+++ src/Command/Drupal/DrupalRemoteFilesMountCommand.php
@@ -5,7 +5,6 @@ namespace Platformsh\Cli\Command\Drupal;
 use Platformsh\Cli\Command\ExtendedCommandBase;
 use Platformsh\Cli\Local\LocalApplication;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
@@ -15,8 +14,8 @@ class DrupalRemoteFilesMountCommand extends ExtendedCommandBase {
   protected function configure() {
     $this->setName('drupal:mount-files')
          ->setAliases(array('mount'))
-         ->setDescription('Mount files from a remote environment (via SSHFS)')
-         ->addOption('app', NULL, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'Specify application(s) to build');
+         ->setDescription('Mount files from a remote environment (via SSHFS)');
+    $this->addAppOption();
     $this->addDirectoryArgument();
     $this->addEnvironmentOption();
     $this->addExample('Mounts remote file share for a Drupal project via SSHFS', '-e environmentID /path/to/project');
@@ -35,21 +34,6 @@ class DrupalRemoteFilesMountCommand extends ExtendedCommandBase {
         $this->_execute($input, $app);
       }
     }
-  }
-
-  /**
-   * Check if the remote file share for the project is already mounted.
-   * @param \Platformsh\Cli\Model\Project
-   * @return bool
-   */
-  private function isMounted($check) {
-    $mounts = file('/proc/mounts');
-    foreach ($mounts as $mount) {
-      if (strpos($mount, $check) > -1) {
-        return TRUE;
-      }
-    }
-    return FALSE;
   }
 
   /**
@@ -77,8 +61,23 @@ class DrupalRemoteFilesMountCommand extends ExtendedCommandBase {
       }
     }
     else {
-      $this->stdErr->writeln("Project $project->id already has remote files mounted to <info>local</info>");
+      $this->stdErr->writeln("Remote files already mounted");
     }
     return 0;
+  }
+
+  /**
+   * Check if the remote file share for the project is already mounted.
+   * @param \Platformsh\Cli\Model\Project
+   * @return bool
+   */
+  private function isMounted($check) {
+    $mounts = file('/proc/mounts');
+    foreach ($mounts as $mount) {
+      if (strpos($mount, $check) > -1) {
+        return TRUE;
+      }
+    }
+    return FALSE;
   }
 }
